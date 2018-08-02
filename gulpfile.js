2ab8c6ee872a2cf541395d72a5eb2801e589720b
Tobias Schramm <tobleminer@gmail.com>
2018-08-02 18:43:15 +0200

    Throw out gulp-sass
    libsass fails quite spectacularly, rely on the original sass implementation here

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require("gulp");
 var util = require("gulp-util");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
-var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var shell = require("gulp-shell");
 
@@ -10,8 +9,11 @@ var config = {
   scripts: "js/_*.js",
   styles: "scss/*.scss",
   styles_main: "scss/loaderror.scss",
-  sass_error_message: "[node-sass] <%= error %> in <%= file %>"
+  sass_error_message: "[node-sass] <%= error %> in <%= file %>",
+  sass_include_paths: require("bourbon").includePaths.join(' -I ')
 }
+
+console.log(config.sass_include_paths)
 
 gulp.task("default", ["scripts", "styles"]);
 
@@ -23,23 +25,16 @@ gulp.task("scripts", function () {
 });
 
 gulp.task("styles", function () {
-  return gulp.src(config.styles_main)
-    .pipe(sass({outputStyle: "compressed", onError: function (e) {
-      data = {
-        file: e.file.replace(__dirname+"/", ""),
-        error: e.message,
-      }
-      util.log(util.colors.red(util.template(config.sass_error_message, data)));
-    }}))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("css/"));
+  return gulp.src("")
+    .pipe(shell('mkdir -p css'))
+    .pipe(shell(`scss -I ${config.sass_include_paths} ${config.styles_main} css/loaderror.css`));
 });
 
 gulp.task("deploy", function () {
   return gulp.src("")
     .pipe(shell("tar -cvzf site.tar.gz *.php *.png *.ico css/* js/loaderror.js"))
-    .pipe(shell("scp -P 5190 site.tar.gz loaderror@loaderror.de:~/public"))
-    .pipe(shell("ssh loaderror@loaderror.de -p 5190 './public/deploy.sh' "))
+    .pipe(shell("scp -P 5190 site.tar.gz loaderror@loaderror.de:~"))
+    .pipe(shell("ssh loaderror@loaderror.de -p 5190 './deploy.sh' "))
 });
 
 gulp.task("watch", function () {
